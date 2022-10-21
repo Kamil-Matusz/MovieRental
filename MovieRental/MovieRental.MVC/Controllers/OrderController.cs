53baef1e220740d96232a31857b19e92b96fd163
Awahir <matuszkamil076@gmail.com>
2022-10-21 21:43:30 +0200

    Adding OrderController & Order View

---
+++ MovieRental/MovieRental.MVC/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRental.MVC.Models;
+using MovieRental.MVC.Services;
+
+namespace MovieRental.MVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderService orderService, ShoppingCart shoppingCart)
+        {
+            _orderService = orderService;
+            _shoppingCart = shoppingCart;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Twoje zamówienie jest puste");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderService.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+
+        }
+
+        [HttpGet]
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Dziękujemy za złożenie zamówienia!";
+            return View();
+        }
+    }
+}
