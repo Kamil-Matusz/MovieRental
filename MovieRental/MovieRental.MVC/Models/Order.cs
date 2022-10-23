20545aa2006bfd51f963f83c98ca54b981987cf2
Awahir <matuszkamil076@gmail.com>
2022-10-23 18:11:39 +0200

    Change Order View

--- MovieRental/MovieRental.MVC/Models/Order.cs
+++ MovieRental/MovieRental.MVC/Models/Order.cs
@@ -9,22 +9,23 @@ namespace MovieRental.MVC.Models
     {
         public int OrderId { get; set; }
 
-        [Display(Name = "First name")]
+        [Required(ErrorMessage = "Pole Imię nie może być puste")]
+        [Display(Name = "Imię")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your last name")]
-        [Display(Name = "Last name")]
+        [Required(ErrorMessage = "Pole Nazwisko nie może być puste")]
+        [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your phone number")]
+        [Required(ErrorMessage = "Pole Numer Telefonu nie może być puste")]
         [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Phone number")]
+        [Display(Name = "Numer telefonu")]
         public string PhoneNumber { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Pole Email nie może być puste")]
         [DataType(DataType.EmailAddress)]
         [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
-            ErrorMessage = "The email address is not entered in a correct format")]
+            ErrorMessage = "Adres email musi mieć określony format")]
         public string Email { get; set; }
         public DateTime OrderPlaced { get; set; }
 
