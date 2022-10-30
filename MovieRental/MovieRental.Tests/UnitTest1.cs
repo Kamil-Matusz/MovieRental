25f366cb263f9ffebbeabe74252878de8128a42c
Awahir <matuszkamil076@gmail.com>
2022-10-30 14:15:26 +0100

    Startup tests

--- MovieRental/MovieRental.Tests/UnitTest1.cs
+++ MovieRental/MovieRental.Tests/UnitTest1.cs
@@ -1,14 +1,17 @@
 using System;
 using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MovieRental.MVC.Controllers;
 
 namespace MovieRental.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Fact]
+        public async void Test1()
+        {
+           
         }
     }
 }
